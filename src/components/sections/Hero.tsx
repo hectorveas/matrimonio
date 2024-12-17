10ed19f7c9cccfb9c76468648f1468606d520d3b
Héctor Veas <32781681+hectorveas@users.noreply.github.com>
2024-12-17 00:50:08 -0300

    change image in hero

--- src/components/sections/Hero.tsx
+++ src/components/sections/Hero.tsx
@@ -6,7 +6,7 @@ export function Hero() {
     <div className="relative min-h-screen flex items-center justify-center">
       <div 
         className="absolute inset-0 bg-cover bg-center bg-no-repeat brightness-50"
-        style={{ backgroundImage: "url('https://images.unsplash.com/photo-1519741497674-611481863552?ixlib=rb-1.2.1&auto=format&fit=crop&w=2000&q=80')" }}
+        style={{ backgroundImage: "url('https://media.discordapp.net/attachments/1086091732237688862/1318423332454137926/header.jpg?ex=676244e9&is=6760f369&hm=6655f170393c95969eaed3c201bbbcbcbca0162c7e631495c24512453e56b547&=&format=webp&width=1678&height=1314')" }}
       />
       <div className="relative text-center text-white z-10 px-4">
         <Heart className="mx-auto w-12 h-12 mb-6 animate-pulse" />
